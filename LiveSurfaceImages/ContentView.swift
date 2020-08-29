d74fca03922403b848dd5702f75a7a2583970d57
Darek Ciesla <darek.ciesla@gmail.com>
2020-08-29 12:18:20 +0200

    Add .gitignore

---
+++ LiveSurfaceImages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiveSurfaceImages
+//
+//  Created by Dariusz Ciesla on 29/08/2020.
+//  Copyright © 2020 Darek Ciesla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
